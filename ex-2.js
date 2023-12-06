a09cdc7e3a9a845c4986a2715c64af60d51bc10d
panatyuki <admiral.dancho@gmail.com>
2023-12-06 12:01:20 +0700

    feat: add complete exercise ex1-2

--- ex-2.js
+++ ex-2.js
@@ -2,6 +2,10 @@
 
 function atLeastFive(array, operation) {
   // Start coding here
+  for (let i = 0; i < array.length; i++) {
+    let filterArray = array.filter(operation);
+    return filterArray.length >= 5;
+  }
 }
 
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
@@ -10,6 +14,10 @@ const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
 // Using `atLeastFive` function here
 
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+let scoreRoom1Result = atLeastFive(studentScoresRoom1, (score) => score > 70);
+let scoreRoom2Result = atLeastFive(studentScoresRoom2, (score) => score > 70);
+let scoreRoom3Result = atLeastFive(studentScoresRoom3, (score) => score > 70);
+
+console.log(scoreRoom1Result); // true
+console.log(scoreRoom2Result); // false
+console.log(scoreRoom3Result); // true
